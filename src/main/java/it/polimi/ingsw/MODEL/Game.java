f117680ed22c47eac82555750a2422b751dc3b83
Doot64 <davide.pellegrino37@gmail.com>
2022-03-15 15:19:27 +0100

    Reworked constructors in Game class.

--- src/main/java/it/polimi/ingsw/MODEL/Game.java
+++ src/main/java/it/polimi/ingsw/MODEL/Game.java
@@ -1,18 +1,18 @@
 package it.polimi.ingsw.MODEL;
 
 public class Game {
-    private Player player1, player2, player3, player4;
-    private Cloud cloud1, cloud2;
-    private MotherNature motherNature;
-    private Cards[] cards1, cards2, cards3, cards4;
-    private Gate gate1, gate2, gate3, gate4;
-    private Character[] characters;
-    private Bag bag;
-    private Hall hall1, hall2, hall3, hall4;
-    private Student[] students;
-    private Tower[] towers1, towers2, towers3;
-    private Island[] islands;
-    private Color Green, Red, Blue, Yellow, Pink;
+    public Player player1, player2, player3, player4;
+    public Cloud cloud1, cloud2;
+    public MotherNature motherNature;
+    public Cards[] cards1, cards2, cards3, cards4;
+    public Gate gate1, gate2, gate3, gate4;
+    public Character[] characters;
+    public Bag bag;
+    public Hall hall1, hall2, hall3, hall4;
+    public Student[] students;
+    public Tower[] towers1, towers2, towers3;
+    public Island[] islands;
+    public Color Green, Red, Blue, Yellow, Pink;
 
     public Game(int player_count) {
         Green= new Color(Colors.GREEN);
@@ -27,42 +27,32 @@ public class Game {
             this.cloud2 = new Cloud();
 
             this.motherNature = new MotherNature();
-            this.ingresso1 = new Gate(player1);
-            this.ingresso2 = new Gate(player2);
-            this.ingresso3 = new Gate(player3);
+            this.gate1 = new Gate(player1);
+            this.gate2 = new Gate(player2);
             for(int i=0; i<4; i++)
-                this.personaggi[i] = new Character();
-            for(int i=0; i<5; i++)
-                this.professori[i] = new Professore(i);
-            this.sacchetto = new Bag();
-            this.sala1 = new Hall();
-            this.sala2 = new Hall();
-            this.sala3 = new Hall();
-            for(int i=0; i<131; i++)
-                this.studenti[i] = new Student();
-            this.torre1 = new Tower();
-            this.torre2 = new Tower();
-            this.torre3 = new Tower();
+                this.characters[i] = new Character();
+            this.bag = new Bag();
+            this.hall1 = new Hall();
+            this.hall2 = new Hall();
+            /*for(int i=0; i<131; i++)
+                this.students[i] = new Student();    ----sta roba conviene metterla direttamente in bag*/
             for(int i=0; i<13; i++)
-                this.isole[i] = new Island(i);
+                this.islands[i] = new Island(i);
         }
         else if(player_count==3){
             this.player1 = player1;
             this.player2 = player2;
             this.player3 = player3;
-            this.player4 = player4;
-            int flag=1;
-            this.cloud1 = new Cloud(flag);
-            this.cloud2 = new Cloud(flag);
+            int pflag=1;
+            this.cloud1 = new Cloud(pflag);
+            this.cloud2 = new Cloud(pflag);
 
-            this.madreNatura = new MotherNature();
-            this.ingresso1 = new Gate(player1);
-            this.ingresso2 = new Gate(player2);
-            this.ingresso3 = new Gate(player3);
+            this.motherNature = new MotherNature();
+            this.gate1 = new Gate(player1);
+            this.gate2 = new Gate(player2);
+            this.gate3 = new Gate(player3);
             for(int i=0; i<4; i++)
-                this.personaggi[i] = new Character();
-            for(int i=0; i<5; i++)
-                this.professori[i] = new Professore(i);
+                this.characters[i] = new Character();
             this.sacchetto = new Bag();
             this.sala1 = new Hall();
             this.sala2 = new Hall();
@@ -84,9 +74,9 @@ public class Game {
             this.cloud2 = new Cloud();
 
             this.madreNatura = new MotherNature();
-            this.ingresso1 = new Gate(player1);
-            this.ingresso2 = new Gate(player2);
-            this.ingresso3 = new Gate(player3);
+            this.gate1 = new Gate(player1);
+            this.gate2 = new Gate(player2);
+            this.gate3 = new Gate(player3);
             for(int i=0; i<4; i++)
                 this.personaggi[i] = new Character();
             for(int i=0; i<5; i++)
