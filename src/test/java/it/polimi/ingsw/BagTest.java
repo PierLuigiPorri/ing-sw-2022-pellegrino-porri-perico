b34bb74e68607b363b904c65a60bb4d9520934b1
Pier Luigi Porri <pierluigi.porri@mail.polimi.it>
2022-04-03 20:37:48 +0200

    Completed the management of exceptions. Tested 2 classes correctly.

---
+++ src/test/java/it/polimi/ingsw/BagTest.java
@@ -0,0 +1,80 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.EXCEPTIONS.ImpossibleActionException;
+import it.polimi.ingsw.MODEL.*;
+
+import junit.framework.TestCase;
+import org.junit.*;
+
+public class BagTest {
+    private Bag bag;
+    private Game game;
+
+
+    @Test
+    public void addStudent () {
+        ColorTracker color1= new ColorTracker(Color.RED);
+        ColorTracker color2= new ColorTracker(Color.BLUE);
+        ColorTracker color3= new ColorTracker(Color.GREEN);
+        ColorTracker color4= new ColorTracker(Color.YELLOW);
+        ColorTracker color5= new ColorTracker(Color.PINK);
+
+        Assert.assertEquals(120, bag.getSize());
+
+        Student student1=new Student(color1);
+        Student student2=new Student(color2);
+        Student student3=new Student(color3);
+        Student student4=new Student(color4);
+        Student student5=new Student(color5);
+
+        try {
+            bag.addStudent(color1);
+            bag.addStudent(color1);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student1.getColor().getColor());
+            bag.addStudent(color2);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student2.getColor().getColor());
+            bag.addStudent(color3);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student3.getColor().getColor());
+            bag.addStudent(color5);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student5.getColor().getColor());
+            bag.addStudent(color4);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student4.getColor().getColor());
+            bag.addStudent(color4);
+            bag.addStudent(color4);
+            Assert.assertEquals(bag.extractStudent().getColor().getColor(), student4.getColor().getColor());
+            Assert.assertEquals(122, bag.getSize());
+        }catch (ImpossibleActionException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void extractStudent() {
+        Assert.assertEquals(120, bag.getSize());
+
+        try {
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+            bag.extractStudent();
+        }catch (ImpossibleActionException e){
+            System.out.println(e.getMessage());
+        }
+        Assert.assertEquals(112, bag.getSize());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        game=new Game(2, 0, "Pier", null, "Paolo", null, null, null);
+        bag=new Bag(game);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
