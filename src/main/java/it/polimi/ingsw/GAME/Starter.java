f6b4c7ba28b0f37e8c58b6e16084a45a9b788c97
fedeperico <federico.perico@mail.polimi.it>
2022-04-26 16:32:05 +0200

    Reverted some changes

--- src/main/java/it/polimi/ingsw/GAME/Starter.java
+++ src/main/java/it/polimi/ingsw/GAME/Starter.java
@@ -2,9 +2,6 @@ package it.polimi.ingsw.GAME;
 
 import it.polimi.ingsw.EXCEPTIONS.GameException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.net.Socket;
 
