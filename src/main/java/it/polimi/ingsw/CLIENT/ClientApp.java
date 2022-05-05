e3a0eb52502d0a85d278c6b9dc2dc598ead21368
fedeperico <federico.perico@mail.polimi.it>
2022-05-05 17:21:12 +0200

    Beginning of ClientMsgHandler and related things

--- src/main/java/it/polimi/ingsw/CLIENT/ClientApp.java
+++ src/main/java/it/polimi/ingsw/CLIENT/ClientApp.java
@@ -1,49 +1,45 @@
 package it.polimi.ingsw.CLIENT;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientApp {
+
+    private static ClientMsgHandler msgHandler;
+
     public static void main(String args[]){
-        Socket socket=null;
-        ObjectInputStream in=null;
-        ObjectOutputStream out=null;
+        msgHandler=new ClientMsgHandler("127.0.0.1", 4000); //Connection setup with this IP and Port numbers
+        //TODO: CLI or GUI?
+        cli();
+    }
+
+    private static void cli(){
         int g; //0=New Game, 1=Join Game
-        try {
-            socket = new Socket("127.0.0.1", 4000);
-            System.out.println("Connected!");
-        }
-        catch(Exception e){
-            System.out.println("Connection failed");
-            System.exit(0);
-        }
-        try {
-            out = new ObjectOutputStream(socket.getOutputStream());
-            in = new ObjectInputStream(socket.getInputStream());
-        }
-        catch (Exception e){
-            System.out.println("Stream connection failed");
-            System.exit(0);
-        }
         //New Game or Join Game?
         g=getCorrectInput("Digit 0 to start a New Game or 1 to Join a game", 0, 1);
-        try {
-            out.writeInt(g);
-            out.flush();
-            out.reset(); //TODO: Ricordarsi di farlo sempre
-        }
-        catch (Exception e){
-            System.out.println("Stream connection failed");
-            System.exit(0);
-        }
         if(g==0){
-            newGame(out);
+            newGame();
         }
         else if(g==1){
-
+            joinGame();
         }
+    }
+
+    private static void newGame(){
+        String nick;
+        int gt; //Game Type
+        int np; //Number of players
+        //Nickname request
+        nick=getValidString("Enter your nickname");
+        //Game Type request
+        gt=getCorrectInput("Digit 0 to use simplified rules or 1 to use expert rules", 0, 1);
+        //Number of players request
+        np=getCorrectInput("Digit 2 for a two-player game or 3 for a three-player game", 2, 3);
+        msgHandler.newGame(nick, gt, np); //TODO:Probabilmente qua va fatto un try catch o comunque va gestita la risposta affermativa o negativa
+        System.out.println("Waiting for other players and for the creation of the game");
+    }
+
+    private static void joinGame(){
+
     }
 
     private static int getCorrectInput(String request, int a, int b){
@@ -90,42 +86,5 @@ public class ClientApp {
             }
         }
         return input;
-    }
-
-    private static void newGame(ObjectOutputStream out){
-        String nick;
-        int gt; //Game Type
-        int np; //Number of players
-        //Nickname request
-        nick=getValidString("Enter your nickname");
-        try {
-            out.writeObject(nick);
-            out.flush();
-        }
-        catch (Exception e){
-            System.out.println("Stream connection failed");
-            System.exit(0);
-        }
-        //Game Type request
-        gt=getCorrectInput("Digit 0 to use simplified rules or 1 to use expert rules", 0, 1);
-        try {
-            out.writeInt(gt);
-            out.flush();
-        }
-        catch (Exception e){
-            System.out.println("Stream connection failed");
-            System.exit(0);
-        }
-        //Number of players request
-        np=getCorrectInput("Digit 2 for a two-player game or 3 for a three-player game", 2, 3);
-        try {
-            out.writeInt(np);
-            out.flush();
-        }
-        catch (Exception e){
-            System.out.println("Stream connection failed");
-            System.exit(0);
-        }
-        System.out.println("In attesa degli altri giocatori e della creazione della partita");
     }
 }
--- src/main/java/it/polimi/ingsw/CLIENT/ClientMsgHandler.java
+++ src/main/java/it/polimi/ingsw/CLIENT/ClientMsgHandler.java
@@ -1,4 +1,36 @@
 package it.polimi.ingsw.CLIENT;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientMsgHandler {
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    public ClientMsgHandler(String host, int port){
+        try {
+            socket = new Socket(host, port);
+            System.out.println("Connected!");
+        }
+        catch(Exception e){
+            System.out.println("Connection failed");
+            System.exit(0);
+        }
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+        }
+        catch (Exception e){
+            System.out.println("Stream connection failed");
+            System.exit(0);
+        }
+    }
+
+    public void newGame(String nick, int gt, int np){
+        //TODO:
+        //Si occupa di creare un messaggio di tipo newGame con i parametri specificati
+        //e poi fa out.writeObject(Messaggio); per inviarlo
+        //Inoltre va gestito il meccanismo di ricezione delle risposte
+    }
 }
