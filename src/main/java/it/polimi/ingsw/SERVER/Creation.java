2a45d8011b1ae9b997b731947ed40c801d67dc7b
fedeperico <federico.perico@mail.polimi.it>
2022-05-06 17:41:13 +0200

    Worked on Starter and related classes

--- src/main/java/it/polimi/ingsw/SERVER/Creation.java
+++ src/main/java/it/polimi/ingsw/SERVER/Creation.java
@@ -5,32 +5,48 @@ public class Creation {
     private int nPlayers;
     private int gametype; //0: simplified rules, 1: expert rules
     private int nJoined;
+    private int nReady;
     private String nick1;
     private String nick2=null;
     private String nick3=null;
-    private int nReady;
     private MsgHandler mh1;
     private MsgHandler mh2;
     private MsgHandler mh3;
 
-    public Creation(int id, int ng, int gt, String nick){
+    public Creation(int id, int ng, int gt, String nick, MsgHandler mh){
         nPlayers=ng;
         gametype=gt;
         nJoined=1;
         nick1=nick;
         nReady=1;
+        mh1=mh;
     }
 
     public void setnJoined() {
         this.nJoined++;
     }
 
+    public void setnReady (){
+        this.nReady++;
+    }
     public void setNick2(String nick) {
         this.nick2 = nick;
     }
 
     public void setNick3(String nick) {
         this.nick3 = nick;
+    }
+
+    public void setMh2(MsgHandler mh2) {
+        this.mh2 = mh2;
+    }
+
+    public void setMh3(MsgHandler mh3) {
+        this.mh3 = mh3;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getnPlayers(){
@@ -43,6 +59,10 @@ public class Creation {
 
     public int getnJoined() {
         return nJoined;
+    }
+
+    public int getnReady() {
+        return nReady;
     }
 
     public String getNick1() {
--- src/main/java/it/polimi/ingsw/SERVER/MsgHandler.java
+++ src/main/java/it/polimi/ingsw/SERVER/MsgHandler.java
@@ -16,8 +16,6 @@ public class MsgHandler implements Runnable{
     private Controller controller; //Starter will set this field for every MessageHandler involved when Game and Controller are created
     private String playerName; //The nickname of the player associated to this MessageHandler
     //TODO: Quando la partita inizia per davvero vanno settati tutti i playerName dei MessageHandler
-    //IN ALTERNATIVA: possiamo decidere di passare this (l'intero MH) a Controller ogni volta che chiamiamo qualsiasi funzione
-    //del controller, e a questo punto richiamiamo direttamente il mh passato come parametro per rispondergli
 
     public MsgHandler(Socket socket){
         this.clientSocket=socket;
