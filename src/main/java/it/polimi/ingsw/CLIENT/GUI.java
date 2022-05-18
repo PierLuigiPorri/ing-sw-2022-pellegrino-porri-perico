ad39baf8959ac69d30ff1a1a1521403c25aa9ab5
DavidePellegrino64 <davideedoardo.pellegrino@mail.polimi.it>
2022-05-18 21:07:10 +0200

    CLI work.

--- src/main/java/it/polimi/ingsw/CLIENT/GUI.java
+++ src/main/java/it/polimi/ingsw/CLIENT/GUI.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.CLIENT;
 
+import it.polimi.ingsw.MESSAGES.MessageType;
+
 public class GUI implements View{
     private ClientMsgHandler msgHandler;
 
@@ -17,4 +19,9 @@ public class GUI implements View{
     public void cloudToGate(){}
 
     public void playCard(){}
+
+    @Override
+    public void relay(MessageType msg) {
+
+    }
 }
