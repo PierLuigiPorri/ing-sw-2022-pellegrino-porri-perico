f4bd0a291006bf67aa813b8fe93ebc736a0846e7
fedeperico <federico.perico@mail.polimi.it>
2022-06-03 13:55:40 +0200

    Merge

---
+++ src/main/java/it/polimi/ingsw/CLIENT/GuiApp.java
@@ -0,0 +1,105 @@
+package it.polimi.ingsw.CLIENT;
+
+import it.polimi.ingsw.MESSAGES.MessageType;
+import it.polimi.ingsw.MESSAGES.ResponseMessage;
+import it.polimi.ingsw.MESSAGES.UpdateMessage;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+public class GUIAPP extends Application implements View {
+    private final Object lock=new Object();
+    private final ClientMsgHandler msgHandler = new ClientMsgHandler("127.0.0.1", 4000, lock);
+
+
+    public static void main(String[] args) {
+        GUIAPP GUI=new GUIAPP();
+        launch(args);
+    }
+
+    public GUIAPP(){
+        AckSender ackSender = new AckSender(msgHandler, 5000);
+        msgHandler.setView(this);
+        new Thread(ackSender).start();
+        new Thread(msgHandler).start();
+    }
+
+    @Override
+    @FXML
+    public void start(Stage primaryStage) throws Exception {
+        MainMenuController.setGUI(this);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/mainMenu.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        Image icon=new Image("Graphical_Assets/sfondo.jpg");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("Eriantys");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public void send(MessageType message){
+        msgHandler.send(message);
+    }
+
+    public void waitForMessage(){
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<ResponseMessage> getResponses(){
+        return msgHandler.getResponses();
+    }
+
+    public ArrayList<UpdateMessage> getUpdates(){
+        return msgHandler.getUpdates();
+    }
+
+    @Override
+    public void moveMotherNature() {
+
+    }
+
+    @Override
+    public void gateToIsland() {
+
+    }
+
+    @Override
+    public void gateToHall() {
+
+    }
+
+    @Override
+    public void cloudToGate() {
+
+    }
+
+    @Override
+    public void playCard() {
+
+    }
+
+    @Override
+    public void update(UpdateMessage update) {
+
+    }
+
+    @Override
+    public void setKill() {
+
+    }
+}
